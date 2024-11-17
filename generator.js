360b6d5345c0848d60a5ee9d5efd054d45d52eba
Brayan Hernandez <hbrayan148@gmail.com>
2024-11-17 15:56:25 -0600

    add: make the .env and secret key

--- generator.js
+++ generator.js
@@ -1,6 +1,16 @@
 const crypto = require("crypto")
+const fs = require("fs")
+
 
 const secretKey = crypto.randomBytes(32).toString('hex')
 
-// This code generate a secret key o signature
-console.log(`SECRET=${secretKey}`)
+// // This code generate a secret key o signature
+// console.log(`SECRET=${secretKey}`)
+
+// This code  generate a signature
+fs.writeFile('.env', `SECRET=${secretKey}`, (err) => {
+    if (err) return console.log(err)
+
+    console.log("File .env and  secret key created")
+
+})
